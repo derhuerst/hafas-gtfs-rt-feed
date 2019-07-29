544b4da1669459b7f136c56ac54d17485fcc0edd
Jannis R <mail@jannisr.de>
2019-07-29 22:15:27 +0200

    better example :memo:

--- example.js
+++ example.js
@@ -6,21 +6,30 @@ const encodeChunks = require('length-prefixed-stream/encode')
 const {inspect} = require('util')
 const createGtfsRtFeed = require('.')
 
-const hafas = createHafas('hafas-gtfs-rt-feed example')
-const monitor = createMonitor(hafas, {
+const potsdamerPlatz = {
 	north: 52.52,
-	west: 13.36,
+	west: 13.37,
 	south: 52.5,
 	east: 13.39
-})
-
-const feed = createGtfsRtFeed(monitor)
-feed.pipe(encodeChunks()).pipe(process.stdout)
-
-// const feed = createGtfsRtFeed(monitor, {debug: true})
-// feed.on('data', (msg) => {
-// 	console.log(inspect(msg, {depth: null, colors: true}))
+}
+const centerOfBerlin = {
+	north: 52.55,
+	west: 13.31,
+	south: 52.48,
+	east: 13.48
+}
+const hafas = createHafas('hafas-gtfs-rt-feed example')
+const monitor = createMonitor(hafas, centerOfBerlin, 20 * 1000)
+// monitor.on('stats', (stats) => {
+// 	console.error(stats)
 // })
+
+const feed = createGtfsRtFeed(monitor, {debug: true})
+feed.on('data', (msg) => {
+	console.log(inspect(msg, {depth: null, colors: true}))
+})
+// const feed = createGtfsRtFeed(monitor)
+// feed.pipe(encodeChunks()).pipe(process.stdout)
 
 feed.once('error', (err) => {
 	console.error(err)
