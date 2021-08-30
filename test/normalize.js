b5444dd9255d61012d7e7e001f79a9dad0048566
Jannis R <mail@jannisr.de>
2021-08-30 17:15:52 +0200

    tokenize-vbb-station-name -> normalize-vbb-station-name-for-search; 4.3.0

--- test/normalize.js
+++ test/normalize.js
@@ -1,11 +1,7 @@
 'use strict'
 
-const tokenize = require('tokenize-vbb-station-name')
+const normalizeStopName = require('normalize-vbb-station-name-for-search')
 const slugg = require('slugg')
-
-const normalizeStopName = (name) => {
-	return tokenize(name, {meta: 'remove'}).join('-')
-}
 
 // we match hafas-client here
 // https://github.com/public-transport/hafas-client/blob/8ed218f4d62a0c220d453b1b1ffa7ce232f1bb83/parse/line.js#L13
