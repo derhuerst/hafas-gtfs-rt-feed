9b437bfcd0c1f6017eb3dfe9e5e461fe8d40406d
Jannis R <mail@jannisr.de>
2021-10-11 16:19:28 +0200

    expose "before matching" hook via CLI; 5.3.0

--- match.js
+++ match.js
@@ -14,9 +14,15 @@ const argv = mri(process.argv.slice(2), {
 if (argv.help || argv.h) {
 	process.stdout.write(`
 Usage:
-    match-with-gtfs <path-to-hafas-info> <path-to-gtfs-info>
+    match-with-gtfs <path-to-hafas-info> <path-to-gtfs-info> [options]
+Options:
+    --before-match-trip      Path to a hook function that modifies each trip
+                             before it is being matched.
+    --before-match-movement  Path to a hook function that modifies each movement
+                             before it is being matched.
 Examples:
     match-with-gtfs hafas-info.js gtfs-info.js
+    match-with-gtfs hafas-info.js gtfs-info.js --before-match-movement fix-movement.js
 \n`)
 	process.exit(0)
 }
@@ -42,4 +48,22 @@ const pathToGtfsInfo = argv._[1]
 if (!pathToGtfsInfo) showError('Missing path-to-gtfs-info argument.')
 const gtfsInfo = require(pathResolve(process.cwd(), pathToGtfsInfo))
 
-runMatching(hafasInfo, gtfsInfo)
+const opt = {}
+
+if (argv['before-match-trip']) {
+	const p = argv['before-match-trip']
+	opt.beforeMatchTrip = require(pathResolve(process.cwd(), p))
+	if ('function' !== typeof opt.beforeMatchTrip) {
+		showError('File specified by --before-match-trip does not export a function.')
+	}
+}
+
+if (argv['before-match-movement']) {
+	const p = argv['before-match-movement']
+	opt.beforeMatchMovement = require(pathResolve(process.cwd(), p))
+	if ('function' !== typeof opt.beforeMatchMovement) {
+		showError('File specified by --before-match-movement does not export a function.')
+	}
+}
+
+runMatching(hafasInfo, gtfsInfo, opt)
