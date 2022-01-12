e725a06d694effe613ba11b070dcc246be689758
Jannis R <mail@jannisr.de>
2022-01-12 18:03:00 +0100

    monitor: add --trips-fetch-mode

--- monitor.js
+++ monitor.js
@@ -27,6 +27,19 @@ Options:
         With \`--movements-fetch-mode "on-demand"\`, when the \`serve-as-gtfs-rt\` component
         has signalled demand, for how long shall movements be fetched?
         Default: movements fetching interval (60s by default) * 5
+    --trips-fetch-mode <mode>
+        Control when trips are fetched from HAFAS.
+        "never":
+            Never fetch a movement's respective trip.
+        "on-demand":
+            Only fetch movements' respective trips from HAFAS when the \`serve-as-gtfs-rt\`
+            component has signalled demand.
+        "continuously" (default):
+            Always fetch each movement's respective trip.
+    --trips-demand-duration <milliseconds>
+        With \`--trips-fetch-mode "on-demand"\`, when the \`serve-as-gtfs-rt\` component
+        has signalled demand, for how long shall trips be fetched?
+        Default: movements fetching interval (60s by default) * 2
 Examples:
     monitor-hafas my-hafas-client.js -d trips '{"north": 1.1, "west": 22.2, "south": 3.3, "east": 33.3}'
 \n`)
@@ -59,6 +72,12 @@ if (argv['movements-fetch-mode']) {
 }
 if (argv['movements-demand-duration']) {
 	opt.movementsDemandDuration = parseInt(argv['movements-demand-duration'])
+}
+if (argv['trips-fetch-mode']) {
+	opt.tripsFetchMode = argv['trips-fetch-mode']
+}
+if (argv['trips-demand-duration']) {
+	opt.tripsDemandDuration = parseInt(argv['trips-demand-duration'])
 }
 
 runMonitor(hafasClient, opt)
